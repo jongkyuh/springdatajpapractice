6df3e5ea798471be9b66d9078dc8a06baa4ed138
jongkyuh <jongkyuh@naver.com>
2024-10-28 21:52:53 +0900

    7. 스프링데이터 JPA 정렬

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,5 +1,8 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -33,6 +36,10 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
     List<Member> findListByUsername(String username);   //컬렉션
     Member findMemberByUsername(String username);       // 단건
     Optional<Member> findOptionalMemberByUsername(String username); //단건 optional
+
+
+    @Query(value = "select m from Member m left join m.team t where m.age = :age",countQuery = "select count(m.id) from Member m")
+    Page<Member> findByAge(@Param("age") int age,Pageable pageable);
 
 
  }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -4,6 +4,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.dto.MemberDto;
@@ -90,6 +94,36 @@ class MemberRepositoryTest {
 
         Optional<Member> findMember = memberRepository.findOptionalMemberByUsername("AAA");
         System.out.println("findMember = " + findMember);
+
+
+    }
+
+    @Test
+    public void paging(){
+
+        //given
+        memberRepository.save(new Member("member1",10));
+        memberRepository.save(new Member("member2",10));
+        memberRepository.save(new Member("member3",10));
+        memberRepository.save(new Member("member4",10));
+        memberRepository.save(new Member("member5",10));
+
+        int age = 10;
+
+        PageRequest pageRequest = PageRequest.of(0,3,Sort.by(Sort.Direction.DESC,"username"));
+
+        Page<Member> page = memberRepository.findByAge(age, pageRequest);
+        Page<MemberDto> map = page.map(m -> new MemberDto(m.getUsername(), m.getId(), null));
+
+        //then
+        List<Member> content = page.getContent();   //조회된 데이터
+        Assertions.assertThat(content.size()).isEqualTo(3); // 조회된 데이터 수
+        Assertions.assertThat(page.getTotalElements()).isEqualTo(5);    // 전체 데이터 수
+        Assertions.assertThat(page.getNumber()).isEqualTo(0);   // 페이지 번호
+        Assertions.assertThat(page.getTotalPages()).isEqualTo(2); // 전체 페이지 번호
+        Assertions.assertThat(page.isFirst()).isTrue();     // 첫번째 항목인가?
+        Assertions.assertThat(page.hasNext()).isTrue();     // 다음 페이지가 있는가?
+
 
 
     }
